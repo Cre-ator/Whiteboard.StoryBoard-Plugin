3cf23f4ec76f149ca480111a7bd38ad560c30f57
Stefan Schwarz <s.schwarz@online.de>
2016-03-29 14:01:18 +0200

    Bugfix

--- StoryBoard/StoryBoard.php
+++ StoryBoard/StoryBoard.php
@@ -8,7 +8,7 @@ class StoryBoardPlugin extends MantisPlugin
       $this->description = '...';
       $this->page = 'config_page';
 
-      $this->version = '1.0.4';
+      $this->version = '1.0.5';
       $this->requires = array
       (
          'MantisCore' => '1.2.0, <= 1.3.99',
