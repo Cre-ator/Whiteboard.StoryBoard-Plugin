8a05a4e39a23a05ecb70039fc3d5d8edf5df1766
Stefan Schwarz <s.schwarz@online.de>
2016-09-09 15:58:17 +0200

    whiteboardmenu access update

--- StoryBoard/core/storyboard_print_api.php
+++ StoryBoard/core/storyboard_print_api.php
@@ -7,34 +7,38 @@ class storyboard_print_api
     *
     * @return string
     */
-   public function getMantisVersion()
+   public function getMantisVersion ()
    {
-      return substr( MANTIS_VERSION, 0, 4 );
+      return substr ( MANTIS_VERSION, 0, 4 );
    }
 
    /**
     * Prints head elements of a page
     * @param $string
     */
-   public function print_page_head( $string )
+   public function print_page_head ( $string )
    {
       echo '<link rel="stylesheet" href="' . STORYBOARD_FILES_URI . 'storyboard.css">';
-      html_page_top1( $string );
-      html_page_top2();
-      if ( plugin_is_installed( 'WhiteboardMenu' ) && file_exists( config_get_global( 'plugin_path' ) . 'WhiteboardMenu' ) )
+      html_page_top1 ( $string );
+      html_page_top2 ();
+      if ( plugin_is_installed ( 'WhiteboardMenu' )
+         && file_exists ( config_get_global ( 'plugin_path' ) . 'WhiteboardMenu' )
+      )
       {
-         require_once ( __DIR__ . '/../../WhiteboardMenu/core/whiteboard_print_api.php' );
-         whiteboard_print_api::printWhiteboardMenu ();
+         require_once ( __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR .
+            'WhiteboardMenu' . DIRECTORY_SEPARATOR . 'core' . DIRECTORY_SEPARATOR . 'wmApi.php' );
+         echo '<link rel="stylesheet" href="plugins/WhiteboardMenu/files/whiteboardmenu.css"/>';
+         wmApi::printWhiteboardMenu ();
       }
-      $this->print_plugin_menu();
+      $this->print_plugin_menu ();
    }
 
    /**
     * Prints the plugin specific menu
     */
-   public function print_plugin_menu()
+   public function print_plugin_menu ()
    {
-      echo '<table align="center">';
+      echo '<table align="center" width="100%">';
       echo '<tr><td colspan="4" class="center" ><font color="#8b0000" size="5">*** Plugin befindet sich in Entwicklungsphase ***</font></td></tr>';
       echo '</table>';
    }
@@ -42,11 +46,11 @@ class storyboard_print_api
    /**
     * Starts a new row in a table
     */
-   public function printRow()
+   public function printRow ()
    {
-      if ( $this->getMantisVersion() == '1.2.' )
+      if ( $this->getMantisVersion () == '1.2.' )
       {
-         echo '<tr ' . helper_alternate_class() . '>';
+         echo '<tr ' . helper_alternate_class () . '>';
       }
       else
       {
@@ -62,28 +66,28 @@ class storyboard_print_api
     * @param $card_story_pt_post
     * @param $card_acc_crit
     */
-   public function printBugViewFields( $card_type, $card_risk, $card_story_pt, $card_story_pt_post, $card_acc_crit )
+   public function printBugViewFields ( $card_type, $card_risk, $card_story_pt, $card_story_pt_post, $card_acc_crit )
    {
-      $this->printRow();
-      echo '<td class="form-title" colspan="6">' . plugin_lang_get( 'menu_title' ) . '</td>';
+      $this->printRow ();
+      echo '<td class="form-title" colspan="6">' . plugin_lang_get ( 'menu_title' ) . '</td>';
       echo '</tr>';
 
-      $this->printRow();
-      echo '<td class="category" colspan="1">' . plugin_lang_get( 'card_type' ) . '</td>';
+      $this->printRow ();
+      echo '<td class="category" colspan="1">' . plugin_lang_get ( 'card_type' ) . '</td>';
       echo '<td colspan="5" id="card_type">' . $card_type . '</td>';
       echo '</tr>';
 
-      $this->printRow();
-      echo '<td class="category" colspan="1">' . plugin_lang_get( 'card_risk' ) . '</td>';
+      $this->printRow ();
+      echo '<td class="category" colspan="1">' . plugin_lang_get ( 'card_risk' ) . '</td>';
       echo '<td colspan="1" id="card_risk">' . $card_risk . '</td>';
-      echo '<td class="category" colspan="1">' . plugin_lang_get( 'card_story_pt' ) . '</td>';
+      echo '<td class="category" colspan="1">' . plugin_lang_get ( 'card_story_pt' ) . '</td>';
       echo '<td colspan="3" id="card_story_pt">' . $card_story_pt . '</td>';
       echo '</tr>';
 
-      $this->printRow();
-      echo '<td class="category">' . plugin_lang_get( 'card_story_pt_post' ) . '</td>';
+      $this->printRow ();
+      echo '<td class="category">' . plugin_lang_get ( 'card_story_pt_post' ) . '</td>';
       echo '<td colspan="1" id="card_story_pt_post">' . $card_story_pt_post . '</td>';
-      echo '<td class="category">' . plugin_lang_get( 'card_acc_crit' ) . '</td>';
+      echo '<td class="category">' . plugin_lang_get ( 'card_acc_crit' ) . '</td>';
       echo '<td colspan="3" id="card_acc_crit">' . $card_acc_crit . '</td>';
       echo '</tr>';
 
@@ -93,26 +97,26 @@ class storyboard_print_api
    /**
     * Prints the specific plugin fields in the bug-report user interface
     */
-   public function printBugReportFields()
+   public function printBugReportFields ()
    {
-      if ( $this->getMantisVersion() == '1.2.' )
+      if ( $this->getMantisVersion () == '1.2.' )
       {
-         $this->printRow();
-         echo '<td class="form-title" colspan="2">' . plugin_lang_get( 'menu_title' ) . '</td>';
+         $this->printRow ();
+         echo '<td class="form-title" colspan="2">' . plugin_lang_get ( 'menu_title' ) . '</td>';
          echo '</tr>';
       }
       else
       {
-         echo '<legend><span>' . plugin_lang_get( 'menu_title' ) . '</span></legend>';
+         echo '<legend><span>' . plugin_lang_get ( 'menu_title' ) . '</span></legend>';
       }
-      $this->printBugReportSelectInput( 'card_type' );
-      $this->printBugReportTextInput( 'card_risk' );
-      $this->printBugReportTextInput( 'card_story_pt' );
-      $this->printBugReportTextInput( 'card_story_pt_post' );
-      $this->printBugReportTextInput( 'card_acc_crit' );
-      if ( $this->getMantisVersion() == '1.2.' )
+      $this->printBugReportSelectInput ( 'card_type' );
+      $this->printBugReportTextInput ( 'card_risk' );
+      $this->printBugReportTextInput ( 'card_story_pt' );
+      $this->printBugReportTextInput ( 'card_story_pt_post' );
+      $this->printBugReportTextInput ( 'card_acc_crit' );
+      if ( $this->getMantisVersion () == '1.2.' )
       {
-         $this->printRow();
+         $this->printRow ();
          echo '<td class="form-title" colspan="2"></td>';
          echo '</tr>';
       }
@@ -131,34 +135,34 @@ class storyboard_print_api
     * @param $card_story_pt_post
     * @param $card_acc_crit
     */
-   public function printBugUpdateFields( $card_type, $card_risk, $card_story_pt, $card_story_pt_post, $card_acc_crit )
+   public function printBugUpdateFields ( $card_type, $card_risk, $card_story_pt, $card_story_pt_post, $card_acc_crit )
    {
 
-      $this->printRow();
-      echo '<td class="form-title" colspan="6">' . plugin_lang_get( 'menu_title' ) . '</td>';
+      $this->printRow ();
+      echo '<td class="form-title" colspan="6">' . plugin_lang_get ( 'menu_title' ) . '</td>';
       echo '</tr>';
 
-      $this->printBugUpdateSelectInput( 'card_type', $card_type );
-      $this->printBugUpdateTextInput( 'card_risk', $card_risk );
-      $this->printBugUpdateTextInput( 'card_story_pt', $card_story_pt );
-      $this->printBugUpdateTextInput( 'card_story_pt_post', $card_story_pt_post );
-      $this->printBugUpdateTextInput( 'card_acc_crit', $card_acc_crit );
+      $this->printBugUpdateSelectInput ( 'card_type', $card_type );
+      $this->printBugUpdateTextInput ( 'card_risk', $card_risk );
+      $this->printBugUpdateTextInput ( 'card_story_pt', $card_story_pt );
+      $this->printBugUpdateTextInput ( 'card_story_pt_post', $card_story_pt_post );
+      $this->printBugUpdateTextInput ( 'card_acc_crit', $card_acc_crit );
    }
 
    /**
     * @param $input
     */
-   public function printBugReportTextInput( $input )
+   public function printBugReportTextInput ( $input )
    {
-      if ( $this->getMantisVersion() == '1.2.' )
+      if ( $this->getMantisVersion () == '1.2.' )
       {
-         $this->printRow();
+         $this->printRow ();
          echo '<td class="category">';
-         echo '<label><span>' . plugin_lang_get( $input ) . '</span></label>';
+         echo '<label><span>' . plugin_lang_get ( $input ) . '</span></label>';
          echo '</td>';
          echo '<td>';
          echo '<span class="input">';
-         echo '<input ' . helper_get_tab_index() . ' type="text" id="' . $input . '" name="' . $input . '" size="50" maxlength="50" value="" />';
+         echo '<input ' . helper_get_tab_index () . ' type="text" id="' . $input . '" name="' . $input . '" size="50" maxlength="50" value="" />';
          echo '</span>';
          echo '<span class="label-style"></span>';
          echo '</td>';
@@ -167,9 +171,9 @@ class storyboard_print_api
       else
       {
          echo '<div class="field-container">';
-         echo '<label><span>' . plugin_lang_get( $input ) . '</span></label>';
+         echo '<label><span>' . plugin_lang_get ( $input ) . '</span></label>';
          echo '<span class="input">';
-         echo '<input ' . helper_get_tab_index() . ' type="text" id="' . $input . '" name="' . $input . '" size="50" maxlength="50" value="" />';
+         echo '<input ' . helper_get_tab_index () . ' type="text" id="' . $input . '" name="' . $input . '" size="50" maxlength="50" value="" />';
          echo '</span>';
          echo '<span class="label-style"></span>';
          echo '</div>';
@@ -179,27 +183,27 @@ class storyboard_print_api
    /**
     * @param $input
     */
-   public function printBugReportSelectInput( $input )
+   public function printBugReportSelectInput ( $input )
    {
       $storyboard_db_api = new storyboard_db_api();
-      $values = array();
-      $value_rows = $storyboard_db_api->select_all_types();
+      $values = array ();
+      $value_rows = $storyboard_db_api->select_all_types ();
       foreach ( $value_rows as $value_row )
       {
-         $values[] = $value_row[1];
+         $values[] = $value_row[ 1 ];
       }
 
-      if ( $this->getMantisVersion() == '1.2.' )
+      if ( $this->getMantisVersion () == '1.2.' )
       {
-         $this->printRow();
+         $this->printRow ();
          echo '<td class="category">';
-         echo '<label><span>' . plugin_lang_get( $input ) . '</span></label>';
+         echo '<label><span>' . plugin_lang_get ( $input ) . '</span></label>';
          echo '</td>';
          echo '<td>';
          echo '<span class="select">';
-         echo '<select ' . helper_get_tab_index() . ' id="' . $input . '" name="' . $input . '">';
+         echo '<select ' . helper_get_tab_index () . ' id="' . $input . '" name="' . $input . '">';
          echo '<option value=""></option>';
-         if ( !is_null( $values ) )
+         if ( !is_null ( $values ) )
          {
             foreach ( $values as $value )
             {
@@ -213,11 +217,11 @@ class storyboard_print_api
       else
       {
          echo '<div class="field-container">';
-         echo '<label><span>' . plugin_lang_get( $input ) . '</span></label>';
+         echo '<label><span>' . plugin_lang_get ( $input ) . '</span></label>';
          echo '<span class="select">';
-         echo '<select ' . helper_get_tab_index() . ' id="' . $input . '" name="' . $input . '">';
+         echo '<select ' . helper_get_tab_index () . ' id="' . $input . '" name="' . $input . '">';
          echo '<option value=""></option>';
-         if ( !is_null( $values ) )
+         if ( !is_null ( $values ) )
          {
             foreach ( $values as $value )
             {
@@ -235,12 +239,12 @@ class storyboard_print_api
     * @param $input
     * @param $value
     */
-   public function printBugUpdateTextInput( $input, $value )
+   public function printBugUpdateTextInput ( $input, $value )
    {
-      $this->printRow();
-      echo '<td class="category"><label for="' . $input . '">' . plugin_lang_get( $input ) . '</label></td>';
+      $this->printRow ();
+      echo '<td class="category"><label for="' . $input . '">' . plugin_lang_get ( $input ) . '</label></td>';
       echo '<td colspan="5">';
-      echo '<input ' . helper_get_tab_index() . ' type="text" id="' . $input . '" name="' . $input . '" size="50" maxlength="50" value="' . $value . '" />';
+      echo '<input ' . helper_get_tab_index () . ' type="text" id="' . $input . '" name="' . $input . '" size="50" maxlength="50" value="' . $value . '" />';
       echo '</td>';
       echo '</tr>';
    }
@@ -249,23 +253,23 @@ class storyboard_print_api
     * @param $input
     * @param $input_value
     */
-   public function printBugUpdateSelectInput( $input, $input_value )
+   public function printBugUpdateSelectInput ( $input, $input_value )
    {
       $storyboard_db_api = new storyboard_db_api();
-      $values = array();
-      $value_rows = $storyboard_db_api->select_all_types();
+      $values = array ();
+      $value_rows = $storyboard_db_api->select_all_types ();
       foreach ( $value_rows as $value_row )
       {
-         $values[] = $value_row[1];
+         $values[] = $value_row[ 1 ];
       }
 
-      $this->printRow();
-      echo '<td class="category"><label for="' . $input . '">' . plugin_lang_get( $input ) . '</label></td>';
+      $this->printRow ();
+      echo '<td class="category"><label for="' . $input . '">' . plugin_lang_get ( $input ) . '</label></td>';
       echo '<td colspan="5">';
       echo '<span class="select">';
-      echo '<select ' . helper_get_tab_index() . ' id="' . $input . '" name="' . $input . '">';
+      echo '<select ' . helper_get_tab_index () . ' id="' . $input . '" name="' . $input . '">';
       echo '<option value=""></option>';
-      if ( !is_null( $values ) )
+      if ( !is_null ( $values ) )
       {
          foreach ( $values as $value )
          {
